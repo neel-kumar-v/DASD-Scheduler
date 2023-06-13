6a30e6e3077c6f8243f21b30e8a70f178cf73a56
neel-kumar-v <79852376+neel-kumar-v@users.noreply.github.com>
2023-06-13 13:44:01 +0000

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const rewriteSlashToIndexHtml = () => {
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      // rewrite / as index.html
+      server.middlewares.use('/', (req, _, next) => {
+        if (req.url === '/') {
+          req.url = '/index.html';
+        }
+        next();
+      });
+    },
+  };
+};
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          return 'assets/' + assetInfo.name;
+        },
+      },
+    },
+  },
+  appType: 'mpa', // disable history fallback
+  plugins: [
+    rewriteSlashToIndexHtml(),
+  ],
+});
