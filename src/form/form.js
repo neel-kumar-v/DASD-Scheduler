34d8aec3fc5df9fa83a2e4f28f584ea336165ce5
neel-kumar-v <greenworldwins@gmail.com>
2023-08-08 16:08:24 -0400

    switch dashboard and reports page correctly

---
+++ src/form/form.js
@@ -0,0 +1,10 @@
+import { getQueryVariable } from "../util";
+
+let counselor;
+window.onload = function() {
+    console.log("onload function");
+    // getQueryVariable('counselor')
+    //create a tailwind h1 with the var
+    counselor = getQueryVariable('counselor')
+}
+
