7e79d5234cce93804cd1f4f06448eb79c1573799
neel-kumar-v <greenworldwins@gmail.com>
2023-08-08 11:38:55 -0400

    restructure

--- src/components.js
+++ src/components.js
@@ -15,13 +15,13 @@ class Navbar extends HTMLElement {
         DASD
       </a>
       <ul class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2  mx-auto flex items-center w-auto space-x-6">
-        <li><a class="text-sm text-gray-400 hover:text-gray-500" href="/">Home</a></li>
+        <li><a class="text-sm text-gray-400 hover:text-gray-500 hover:font-bold" href="/">Home</a></li>
         <li class="text-gray-300">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
           </svg>
         </li>
-        <li><a class="text-sm text-blue-600 font-bold" href="#">Reports</a></li>
+        <li><a class="text-sm text-gray-400 hover:text-gray-500 hover:font-bold" href="#">Reports</a></li>
 
       </ul>
       <a class="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Log Out</a>
