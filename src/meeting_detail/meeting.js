2d852f96eb8c8f074e91dce7092dbcec8c1e95b5
Soham Katdare <sohamkatdare8@gmail.com>
2023-10-21 17:14:34 -0400

    note saving

--- src/meeting_detail/meeting.js
+++ src/meeting_detail/meeting.js
@@ -2,15 +2,36 @@ import {
     query,
     where,
     collection,
-    getDocs,
+    getDoc,
+    doc,
     getCountFromServer,
   } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { app, db } from "..firebase.js";
 import { formatTime, formatDate, capitalizeFirstLetter } from "../util.js";
 import { meetingId } from '../reports/reports.js'
+import $ from 'jquery'
+const urlParams = new URLSearchParams(window.location.search);
+
+
+$(window).on("load", () => {
+  urlParams.set("id", meetingId)
+  const docSnap = getDoc(doc(db, 'checkin', urlParams.get('id')))
+  const notes = docSnap.data().notes
+  if (notes != ""){
+    $('message').val(notes)
+  }
+})
 
 function generateQueryParams() {
   let link = '/form/';
   // get all firebase information
 }
+
+$('save-notes').on('click', async () => {
+  await setDoc(doc(db, 'checkin', urlParams.get('id')), {
+    notes: $('message').val()
+  })
+
+  location.reload();
+})
